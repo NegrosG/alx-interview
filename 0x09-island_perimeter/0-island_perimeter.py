59dfa707fa4b52d4b2e436422c32624c754ab8e3
Martins Onifade <negrosmartins@gmail.com>
2024-10-10 23:35:23 +0100

    new island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
-"""
-Island Perimeter:
-    returns the perimeter of the island described in grid
-"""
+
+""" island perimeter solution """
 
 
 def island_perimeter(grid):
-    """island perimenter function"""
-    perimeter = 0
+    """global function for island"""
+    visit = set()
+
+    def dfs(i, j):
+        """function for testing the island"""
+        if (i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or
+                grid[i][j] == 0):
+            return 1
+        if (i, j) in visit:
+            return 0
+        visit.add((i, j))
+        perim = dfs(i, j + 1)
+        perim += dfs(i + 1, j)
+        perim += dfs(i, j - 1)
+        perim += dfs(i - 1, j)
+        """ return perim """
+        return perim
+
     for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i-1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j-1] == 1:
-                    perimeter -= 2
-    return perimeter
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                """return the dfs function"""
+                return dfs(i, j)
+    """ If no land cells are found, the perimeter is 0 """
+    return 0
